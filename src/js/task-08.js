c524997af1669d35b77e42ee7fe3b860060b278d
Panchenko Denis <Pandenisko@gmail.com>
2024-03-07 18:10:49 +0200

    3 commit
    3 commit

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,24 @@
+const formRef = document.querySelector('.login-form');
 
+function buttonSubmitHandler(event){
+    event.preventDefault();
+    const formElements = event.target.elements;
+    const formEmail = formElements.email;
+    const formPassword = formElements.password;
+
+    formEmail.required = true;
+    formPassword.required = true;
+    
+    if (!event.target.checkValidity()){
+       return alert("Всі поля повинні бути заповненні!");
+    }
+        console.log(`${formEmail.name}: ${formEmail.value}`);
+        console.log(`${formPassword.name}: ${formPassword.value}`);
+        event.target.reset();
+    // if (formEmail.value === "" || formPassword.value === ""){
+    //     alert("Всі поля повинні бути заповненні!");
+    //     }
+    // так alert буде з'являтись кожного разу, але не буде фрази "заповніть це поле" на пустому полі пароля або пошти.
+};
+
+formRef.addEventListener('submit', buttonSubmitHandler);
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyRef = document.querySelector('body');
+const spanRef = document.querySelector('span.color');
+const buttonRef = document.querySelector('.change-color');
+
+
+function buttonClickHandler(){
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  spanRef.textContent = getRandomHexColor();
+}
+
+buttonRef.addEventListener('click', buttonClickHandler);
